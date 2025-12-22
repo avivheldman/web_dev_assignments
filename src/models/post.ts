d9d2f39435464f915e9762e4b01899d3da24d141
avivh-wonderful <aviv.h@wonderful.ai>
2025-12-22 23:27:51 +0200

    add post endpoint with mongodb

---
+++ src/models/post.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IPost extends Document {
+  title: string;
+  content: string;
+  sender: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const postSchema = new Schema<IPost>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    sender: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<IPost>('Post', postSchema);
+
